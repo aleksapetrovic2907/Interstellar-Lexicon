e3342311ef3bd3a72b386eb88e41ef6911388bdd
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-04-26 19:41:39 +0200

    Created `BottomBlockController` which hides the block when game is over

---
+++ Assets/InterstellarLexicon/Scripts/UI/BottomBlockController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace AP.UI
+{
+    public class BottomBlockController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform block;
+        [SerializeField] private float hideDuration;
+        [SerializeField] private Ease hideEase;
+
+        private void Start() => GameManager.Instance.OnGameOver += HideBlock;
+
+        private void HideBlock()
+        {
+            Vector2 targetPosition = new Vector2(block.anchoredPosition.x, -block.anchoredPosition.y);
+            block.DOAnchorPos(targetPosition, hideDuration).SetEase(hideEase).Play();
+        }
+    }
+}
