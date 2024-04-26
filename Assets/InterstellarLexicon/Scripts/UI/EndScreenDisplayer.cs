59f6ed2a7b43cb45299b01137f5ed80f9400d8b0
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-04-26 19:22:26 +0200

    Implement `EndScreenDisplayer` for when game is over

---
+++ Assets/InterstellarLexicon/Scripts/UI/EndScreenDisplayer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+namespace AP.UI
+{
+    public class EndScreenDisplayer : MonoBehaviour
+    {
+        [SerializeField] private GameObject panel;
+        [SerializeField] private float panelPopupDuration;
+        [SerializeField] private Ease panelPopupEase;
+
+        [Header("Data")]
+        [SerializeField] private TextMeshProUGUI levelTMP;
+        [SerializeField] private TextMeshProUGUI wordsTMP;
+        [SerializeField] private TextMeshProUGUI wpmTMP;
+        [SerializeField] private TextMeshProUGUI maxComboTMP;
+        [SerializeField] private TextMeshProUGUI durationTMP;
+        [SerializeField] private TextMeshProUGUI pointsTMP;
+
+        private void Start() => GameManager.Instance.OnGameOver += DisplayScreen;
+
+        private void DisplayScreen()
+        {
+            // Setup end game data.
+            levelTMP.text = GameManager.Instance.Level.ToString();
+            wordsTMP.text = GameManager.Instance.WordsWrittenCorrectly.ToString();
+            wpmTMP.text = GameManager.Instance.WordsPerMinute.ToString();
+            maxComboTMP.text = GameManager.Instance.HighestCombo.ToString();
+            durationTMP.text = GameManager.Instance.GameDurationInSeconds.ToString();
+            pointsTMP.text = GameManager.Instance.Points.ToString();
+
+            // Animate panel.
+            panel.transform.DOScale(Vector3.one, panelPopupDuration).From(Vector3.zero).SetEase(panelPopupEase).Play();
+            panel.SetActive(true);
+        }
+    }
+}
