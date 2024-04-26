7ec6dd27c29a52bff6380dab4989d91f2e33473e
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-04-26 19:56:57 +0200

    Destroy planet and display visual effect when game is over

--- Assets/InterstellarLexicon/Scripts/_Core/PlanetBehaviour.cs
+++ Assets/InterstellarLexicon/Scripts/_Core/PlanetBehaviour.cs
@@ -8,10 +8,22 @@ namespace AP
     {
         public event Action<Enemy> OnHitByEnemy;
 
+        [SerializeField] private ParticleSystem explosionVFX;
+
+        private void Start() => GameManager.Instance.OnGameOver += DestroyPlanet;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.TryGetComponent<Enemy>(out var enemy)) return;
             OnHitByEnemy?.Invoke(enemy);
+        }
+
+        [ContextMenu("test")]
+        private void DestroyPlanet()
+        {
+            explosionVFX.transform.SetParent(null);
+            explosionVFX.gameObject.SetActive(true);
+            Destroy(gameObject);
         }
     }
 }
